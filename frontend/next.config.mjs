16a5de279ea271e61f26ee755cc69f3059bd4d44
Cak Indra Now <1ndrww09@gmail.com>
2024-11-23 16:59:09 +0800

    update config next

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [{ hostname: "images.pexels.com" } , {hostname : "localhost"}],
+        remotePatterns: [{ hostname: "images.pexels.com" } , {hostname : "localhost"} , {hostname : "88.222.242.99"}],
       },
 }
 export default nextConfig;
